245a3a0bce31cd279e43658a8d0633b326866dec
Camarel <alltime326@gmail.com>
2016-05-15 20:25:26 +0800

    C# for Menustrip_Load_txt_File

---
+++ Menustrip_Load_txt_File/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Menustrip_Load_txt_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(283, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 221);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(86, 255);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 284);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(283, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(128, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 306);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Menustrip_Load_txt_File/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Menustrip_Load_txt_File
+{
+    public partial class Form1 : Form
+    {
+        string filepath;
+        FileInfo info;
+        bool status = false; // false => load true => save
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ToolStripMenuItem MenuFile = new ToolStripMenuItem("檔案(&F)");
+            ToolStripMenuItem MenuClear = new ToolStripMenuItem("清空(&N)");
+            ToolStripMenuItem MenuSave = new ToolStripMenuItem("儲存(&S)");
+            ToolStripMenuItem MenuLoad = new ToolStripMenuItem("讀取檔案");
+            ToolStripMenuItem MenuExit = new ToolStripMenuItem("結束(&X)");
+            ToolStripMenuItem MenuFont = new ToolStripMenuItem("字體");
+            ToolStripMenuItem MenuSize = new ToolStripMenuItem("大小");
+            ToolStripMenuItem MenuSize10 = new ToolStripMenuItem("10");
+            ToolStripMenuItem MenuSize20 = new ToolStripMenuItem("20");
+            ToolStripMenuItem MenuSize30 = new ToolStripMenuItem("30");
+            ToolStripMenuItem MenuColor = new ToolStripMenuItem("顏色");
+            ToolStripMenuItem MenuColorY = new ToolStripMenuItem("黃");
+            ToolStripMenuItem MenuColorG = new ToolStripMenuItem("綠");
+            ToolStripMenuItem MenuColorR = new ToolStripMenuItem("紅");
+
+            MenuFile.ShortcutKeys = Keys.Alt | Keys.F;
+            MenuClear.ShortcutKeys = Keys.Control | Keys.N;
+            MenuSave.ShortcutKeys = Keys.Control | Keys.S;
+            MenuLoad.ShortcutKeys = Keys.Control | Keys.O;
+            MenuExit.ShortcutKeys = Keys.Control | Keys.E;
+
+            MenuFile.DropDownItems.AddRange(new ToolStripItem[] { MenuClear, MenuSave, MenuLoad, MenuExit });
+            MenuSize.DropDownItems.AddRange(new ToolStripItem[] { MenuSize10, MenuSize20, MenuSize30 });
+            MenuColor.DropDownItems.AddRange(new ToolStripItem[] { MenuColorY, MenuColorG, MenuColorR });
+            MenuFont.DropDownItems.AddRange(new ToolStripItem[] { MenuSize, MenuColor });
+
+            menuStrip1.Items.Add(MenuFile);
+            menuStrip1.Items.Add(MenuFont);
+
+            MenuClear.Click += MenuClear_Click;
+            MenuSave.Click += MenuSave_Click;
+            MenuLoad.Click += MenuLoad_Click;
+            MenuExit.Click += MenuExit_Click;
+
+            MenuSize10.Click += MenuSize10_Click;
+            MenuSize20.Click += MenuSize20_Click;
+            MenuSize30.Click += MenuSize30_Click;
+
+            MenuColorY.Click += MenuColorY_Click;
+            MenuColorG.Click += MenuColorG_Click;
+            MenuColorR.Click += MenuColorR_Click;
+
+            progressBar1.Step = 10;
+            toolStripProgressBar1.Step = 10;
+            toolStripStatusLabel1.Text = "";
+
+            EventArgs e = new EventArgs();
+            Form1_Load(this, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filepath = Application.StartupPath + @"\Load.txt";
+
+            info = new FileInfo(filepath);
+
+            //try?
+            if (info.Exists)
+            {
+                StreamReader sr = new StreamReader(filepath);
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+            else
+            {
+                FileStream fs = info.Create();
+                fs.Close();
+                MessageBox.Show("沒有檔案!!新增一個空的Load檔案!!");
+            }
+
+            textBox1.Text ="";
+        }
+
+        private void MenuClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void MenuSave_Click(object sender, EventArgs e)
+        {
+            status = true;
+            timer1.Enabled = true;
+            StreamWriter sw = info.CreateText();
+            sw.Write(textBox1.Text);
+            sw.Flush();
+            sw.Close();
+        }
+
+        private void MenuLoad_Click(object sender, EventArgs e)
+        {
+            status = false;
+            timer1.Enabled = true;
+            StreamReader sr = new StreamReader(filepath);
+            textBox1.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MenuSize10_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font("新細明體", 10.0f);
+        }
+
+        private void MenuSize20_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font("新細明體", 20.0f);
+        }
+
+        private void MenuSize30_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font("新細明體", 30.0f);
+        }
+
+        private void MenuColorY_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Yellow;
+        }
+
+        private void MenuColorG_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Green;
+        }
+
+        private void MenuColorR_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+            if(progressBar1.Value == progressBar1.Maximum)
+            {
+                progressBar1.Value = 0;
+                timer1.Enabled = false;
+            }
+
+            toolStripProgressBar1.PerformStep();
+            
+            toolStripStatusLabel1.Text = status ? "儲存中" + ((toolStripProgressBar1.Value / toolStripProgressBar1.Maximum) * 100).ToString() + "%"
+                                                : "讀取中" + ((toolStripProgressBar1.Value / toolStripProgressBar1.Maximum) * 100).ToString() + "%";
+            
+            if(toolStripProgressBar1.Value == toolStripProgressBar1.Maximum)
+            {
+                toolStripProgressBar1.Value = 0;
+                toolStripStatusLabel1.Text = status ? "儲存完畢" : "讀取完畢"  ;
+                timer1.Enabled = false;
+            }
+        }
+    }
+}
